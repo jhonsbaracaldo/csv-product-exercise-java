4eaa200b462971a79f92227c1ee9d40e815dd9d8
jhonsbaracaldo <jhonsbaracaldo@gmail.com>
2023-11-06 16:44:16 -0500

    CSV

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         readerMavenCSV readerMavenCSV = new readerMavenCSV();
         Scanner sc= new Scanner(System.in);
         // Cargar productos desde un archivo CSV
-        System.out.println("Seleciona por favor que va realizar \n1.Ver Productos\n2.Añadir\n3.Consultar \n4.ReaderCSV");
+        System.out.println("Seleciona por favor que va realizar \n1.Ver Productos\n2.Añadir\n3.remover \n4.ReaderCSV");
 
             int seleccion = 0;
             while (seleccion!=4) {
